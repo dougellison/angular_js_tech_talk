099859de741a3d024022479811ab1a5d0adf09e3
dougellison <doug.ellison@gmail.com>
2013-08-20 21:36:13 -0600

    Added Data-Binding Example

---
+++ examples/data-binding/js/controllers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* Controllers */
+
+var controllers = angular.module('myApp.controllers', []);
+controllers.controller('Databinding', function($scope) {
+	$scope.helloWorld = "Hello there People";
+	
+	$scope.helloMessage = "Type and I will update";
+	
+	
+});
+
+
+
