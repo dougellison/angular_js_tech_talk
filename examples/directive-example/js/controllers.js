8661be19f59bb3c08fa13fd3b4bf90cbbd7f07d3
dougellison <doug.ellison@gmail.com>
2013-08-21 23:45:21 -0600

    Fixed indentation and added left / right brace for directive

--- examples/directive-example/js/controllers.js
+++ examples/directive-example/js/controllers.js
@@ -3,19 +3,19 @@
 /* Controllers */
 
 var controllers = angular.module('myApp.controllers', []);
-controllers.controller('Directive', function($scope) {
+controllers.controller('Directive', ['$scope', function($scope) {
 
-$scope.copyValue = {};
-$scope.copyValue.copyMessage = "Hey I'm a copy";
+	$scope.copyValue = {};
+	$scope.copyValue.copyMessage = "Hey I'm a copy";
 
-$scope.copies = [1,2,3]
+	$scope.copies = [1,2,3]
 
-$scope.parentValue = [1];	
+	$scope.parentValue = [1];	
 	$scope.addNew = function() {
 			$scope.parentValue.push((new Date).getTime());
 	}
 	
-});
+}]);
 
 
 
